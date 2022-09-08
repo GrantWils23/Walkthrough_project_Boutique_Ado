83933116fe3d26f5a1e34ee1d59bb6777de5314c
Grant <GrantWils.23@googlemail.com>
2022-09-08 20:27:43 +0000

    push to heroku retry

--- boutique_ado/settings.py
+++ boutique_ado/settings.py
@@ -190,3 +190,4 @@ DEFAULT_FROM_EMAIL = 'boutiqueado@example.com'
 STRIPE_PUBLIC_KEY = os.environ.get('STRIPE_PUBLIC_KEY', '')
 STRIPE_SECRET_KEY = os.environ.get('STRIPE_SECRET_KEY', '')
 STRIPE_WH_SECRET = os.environ.get('STRIPE_WH_SECRET', '')
+
